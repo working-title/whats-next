ce1401a0f55ab08b29f64c8329514102b365fdc0
Joseph Bennett <josephbennett42@gmail.com>
2015-07-04 15:00:09 +1200

    add helpful comments during setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 # Update
-echo "Updating"
+echo "Updating Apt"
  sudo apt-get update -qq
 
+echo "Upgrading Packages..."
+sudo apt-get upgrade -qq
+
 # Packages
-echo "Installing packages"
+echo "Installing the packages we need :)"
 sudo apt-get install -y -qq git \
 						python-dev \
 						python-pip \
 
 # Setup virtual environment
 sudo pip install virtualenvwrapper
+
+echo 'Configuring Python Virtual Environment'
 
 echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bashrc
 echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
@@ -23,8 +28,12 @@ mkvirtualenv working-title
 echo 'cd /srv/project' >> ~/.bashrc
 echo 'workon working-title' >> ~/.bashrc
 
+
 # Sync Github
+echo 'Cloning our awesome code repo!'
 (cd /srv/project ;
  git config --global push.default simple
  pip install -r requirements.txt
 )
+
+echo 'Done :) - Hopefully!'
