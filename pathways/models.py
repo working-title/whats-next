63df03396f7de7f51b7310c19552a4d93685f99c
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-07-04 17:35:41 +1200

    made maori names optinal

--- pathways/models.py
+++ pathways/models.py
@@ -9,7 +9,7 @@ class Qualification(models.Model):
 class Career(models.Model):
     name = models.CharField(max_length=256)
     income = models.PositiveIntegerField()
-    maori_name = models.CharField(max_length=256)
+    maori_name = models.CharField(max_length=256, blank=True)
     hours_worked_30_less = models.PositiveIntegerField()
     hours_worked_60_less = models.PositiveIntegerField()
     hours_worked_60_more = models.PositiveIntegerField()
@@ -17,10 +17,10 @@ class Career(models.Model):
     def __unicode__(self):              # __unicode__ on Python 2
         return self.name
 
-class Interests(models.Model):
+class Interest(models.Model):
     name = models.CharField(max_length=256)
     careers = models.ManyToManyField(Career)
-    maori_name = models.CharField(max_length=256)
+    maori_name = models.CharField(max_length=256, blank=True)
 
     def __unicode__(self):              # __unicode__ on Python 2
         return self.name
