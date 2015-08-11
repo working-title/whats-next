5ce879cbdba953bedded29e1d351ea25d4a49633
Joseph Bennett <josephbennett42@gmail.com>
2015-08-11 04:35:33 +0000

    add industry model

--- pathways/admin.py
+++ pathways/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Career, Interest, Qualification, Category
+from .models import Career, Interest, Qualification, Category, Industry
 
 admin.site.register(Career)
 admin.site.register(Interest)
 admin.site.register(Qualification)
 admin.site.register(Category)
+admin.site.register(Industry)
