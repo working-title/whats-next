9a19ca39770ed85f9dc7ede69b987378890b4e9f
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-08-11 16:41:34 +1200

    Page Resizing JS "Sorta Working" The javascript resizing the page is now working better

--- static/js/page_resizing.js
+++ static/js/page_resizing.js
@@ -6,7 +6,24 @@ $(window).resize(setHeightPageSections);
 // this height
 function setHeightPageSections() {
 	var height = $(window).height();
+	var width = $(window).width();
 	var pageSections = document.getElementsByClassName("fill-screen");
+	
+	console.log(width);
+	console.log(height);
+	if (width >= 1200 && height < 900) {
+		console.log("lg");
+		height = 900;
+	} else if (width >= 992 && height < 600) {
+		console.log("md");
+		height = 600;
+	} else if (width >= 768 && height < 400) {
+		console.log("sm");
+		height = 400;
+	} else if (width < 768 && height < 900) {
+		console.log("xs");
+		height = 900;
+	}
 	
 	for (var i = 0; i < pageSections.length; i++) {
 		$(pageSections).height(height);	
