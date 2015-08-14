ff133d033e4ec5dae61615dbbbb07a50d3606c1a
Joseph Bennett <josephbennett42@gmail.com>
2015-08-15 09:47:37 +1200

    add basic industry API endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,31 @@
 from rest_framework import serializers
-from pathways.models import Qualification, Career, Interest
+from rest_framework.exceptions import ParseError
+from pathways.models import Qualification, Career, Interest, Industry
 
 
-class QualificationSerializer(serializers.HyperlinkedModelSerializer):
+class TagListSerializer(serializers.Field):
+    """
+    Used to serialize tags made with the taggit library.
+    """
+
+    def to_internal_value(self, data):
+        if type(data) is not list:
+            raise ParseError("expected a list of data")
+        return data
+
+    def to_representation(self, value):
+        if type(value) is not list:
+            return [tag.name for tag in value.all()]
+        return value
+
+
+class QualificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Qualification
         fields = ('name', 'debt')
 
-class CareerSerializer(serializers.HyperlinkedModelSerializer):
+
+class CareerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Career
         fields = ('name',
@@ -21,10 +39,28 @@ class CareerSerializer(serializers.HyperlinkedModelSerializer):
                   'school',
                   'post_school',
                   'degree')
+        depth = 2
 
-class InterestSerializer(serializers.HyperlinkedModelSerializer):
+
+class IndustrySerializer(serializers.ModelSerializer):
+    tags = TagListSerializer()
+
+    class Meta:
+        model = Industry
+        fields = ('name',
+                  'description',
+                  'careers',
+                  'tags',)
+        depth = 2
+
+
+class InterestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Interest
         fields = ('name',
                   'careers',
                   'maori_name')
+        depth = 2
+
+
+
--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
+import json
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
-from pathways.models import Qualification, Career, Interest
-from api.serializers import QualificationSerializer, CareerSerializer, InterestSerializer
+from pathways.models import Qualification, Career, Interest, Industry
+from api.serializers import QualificationSerializer, CareerSerializer, InterestSerializer, IndustrySerializer
 
 
 class QualificationViewSet(viewsets.ModelViewSet):
@@ -11,12 +15,51 @@ class QualificationViewSet(viewsets.ModelViewSet):
     queryset = Qualification.objects.all()
     serializer_class = QualificationSerializer
 
+
 class CareerViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows careers to be viewed.
     """
     queryset = Career.objects.all()
     serializer_class = CareerSerializer
+
+
+class IndustryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows industries to be viewed.
+    """
+    queryset = Industry.objects.all()
+    serializer_class = IndustrySerializer
+    parser_classes = (JSONParser,)
+
+    @list_route(methods=['post'], url_path='from_tags')
+    def from_tags(self, request, pk=None):
+        """
+        Gets the first 3 industries that match the tags posted.
+        """
+
+        if not request.data:
+            return Response({'error': "There was no JSON data posted."})
+
+        try:
+            tags = request.data['tags']
+
+            if len(tags) == 0:
+                return Response({'error': "Expected at least one tag."})
+        except KeyError:
+            return Response({'error': "Expected a JSON list of tags to find industries with under the key 'tags'"})
+
+        # get first 3 industries that have the tags that were posted.
+        # make sure they're distinct so we don't get multiple results if the same
+        # model matches two or more tags
+        industries = Industry.objects.filter(tags__name__in=tags).distinct()[:3]
+
+        if len(industries) == 0:
+            return Response({'error': "No industries matched the given tags."})
+
+        serializer = IndustrySerializer(industries, many=True)
+        return Response(serializer.data)
+
 
 class InterestViewSet(viewsets.ModelViewSet):
     """
