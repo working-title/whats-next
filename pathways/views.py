97c0d11870a9d59d93e2c83f6d2f4a987db67507
Joseph Bennett <josephbennett42@gmail.com>
2015-08-15 10:04:16 +1200

    load tags from backend

--- pathways/views.py
+++ pathways/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render_to_response, render
+from taggit.models import Tag
 from pathways.models import Career, Category, Interest
 
 
 def index(request):
+    tags = Tag.objects.all()
     categories = Category.objects.all()
     interests = Interest.objects.all()
     return render(request, "pathways/index.html", locals())
